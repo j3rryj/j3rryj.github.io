034d3a6e5b431ee91755c8bb5452421a25440415
Jerry Jong <jerryjong01@gmail.com>
2017-11-10 23:00:25 -0800

    attached resume link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,8 +26,8 @@ class Home extends Component {
     return(
       <div style={{textAlign: 'center', paddingTop: '18%'}}>
         <span style={{ whiteSpace: 'pre', letterSpacing: '12px', color: 'white', margin: '0 auto', fontSize: '3.5em', fontWeight: '900'}} ref={(el) => { this.el = el; }}/><br/>
-        <FlatButton labelStyle={{fontSize: '1em', color: 'white'}} label='CONTACT' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%', marginRight: '1%'}}/>
-        <FlatButton labelStyle={{fontSize: '1em', color: 'white'}} label='RESUME' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%'}}/>
+        <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='CONTACT' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%', marginRight: '1%'}}/>
+        <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='RESUME' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%'}}/>
       </div>
     )
   }
