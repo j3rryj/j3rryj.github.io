755ed0c01df041f9b51a1bf2a1be47442430005b
Jerry Jong <jerryjong01@gmail.com>
2017-11-10 23:01:19 -0800

    removed unnecessary routes

--- src/components/routes/route.js
+++ src/components/routes/route.js
@@ -1,25 +1,17 @@
 // routes.js
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './../Home.jsx';
-import Resume from './../Resume.jsx';
-import Services from './../Services.jsx';
 import Portfolio from './../Portfolio.jsx';
-import Contact from './../Contact.jsx';
 import Blog from './../Blog.jsx';
-import About from './../About.jsx';
 
 const routes = (
   <div>
     <Route exact path = '/' component={Home}/>
     <Route exact path = '/home' component={Home}/>
     <Route exact path = '/blog' component={Blog}/>
-    <Route exact path = '/about' component={About}/>
-    <Route exact path = '/resume' component={Resume}/>
-    <Route exact path = '/services' component={Services}/>
     <Route exact path = '/portfolio' component={Portfolio}/>
-    <Route exact path = '/contact' component={Contact}/>
   </div>
 )
 
