fded7adcaea6ae86de98a3adc7e3a00bfdfbb78e
Jerry Jong <jerryjong01@gmail.com>
2017-11-13 14:02:14 -0800

    refactored typedjs style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import FlatButton from 'material-ui/FlatButton';
 
 import Typed from 'typed.js';
 
@@ -27,10 +26,12 @@ class Home extends Component {
 
   render() {
     return(
-      <div style={{textAlign: 'center', paddingTop: '18%'}}>
-        <span style={{whiteSpace: 'pre', letterSpacing: '12px', color: 'white', margin: '0 auto', fontSize: '3.5em', fontWeight: '900'}} ref={(el) => { this.el = el; }}/><br/>
-        <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='CONTACT' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%', marginRight: '1%'}}/>
-        <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='RESUME' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%'}}/>
+      <div className="home-div">
+        <span className="typer" ref={(el) => { this.el = el; }}/><br/>
+        <div className="home-div-contact">
+        <button className="home-div-contact-btn" href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0">Contact</button>
+        <button className="home-div-contact-btn" href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0">Resume</button>
+        </div>
       </div>
     )
   }
