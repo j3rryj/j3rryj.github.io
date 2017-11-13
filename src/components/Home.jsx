0433dc17289a1846233e36d85dbbfe6d556e7859
Jerry Jong <jerryjong01@gmail.com>
2017-11-12 17:42:48 -0800

    changed display strings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,12 @@ import Typed from 'typed.js';
 
 class Home extends Component {
   componentDidMount() {
-    let strings = ["HELLO WORLD.",
-                   "JERRY JONG.",
-                   "DEVELOPER."]
+    let strings = [
+                  "JERRY JONG.",
+                  "DEVELOPER.",
+                  "BLOGGER.",
+                  "ENTREPENEUR."
+                  ]
     const options = {
       strings: strings,
       typeSpeed: 70,
@@ -25,7 +28,7 @@ class Home extends Component {
   render() {
     return(
       <div style={{textAlign: 'center', paddingTop: '18%'}}>
-        <span style={{ whiteSpace: 'pre', letterSpacing: '12px', color: 'white', margin: '0 auto', fontSize: '3.5em', fontWeight: '900'}} ref={(el) => { this.el = el; }}/><br/>
+        <span style={{whiteSpace: 'pre', letterSpacing: '12px', color: 'white', margin: '0 auto', fontSize: '3.5em', fontWeight: '900'}} ref={(el) => { this.el = el; }}/><br/>
         <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='CONTACT' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%', marginRight: '1%'}}/>
         <FlatButton href="https://www.dropbox.com/s/wulobj7z2wizjjg/Jerry%20Jong%20Resume.pdf?dl=0" labelStyle={{fontSize: '1em', color: 'white'}} label='RESUME' style={{display: 'inline-block', border: '1px solid white', marginTop: '1%'}}/>
       </div>
